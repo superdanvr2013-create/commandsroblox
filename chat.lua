98581d6b5ad314087e200392f335a52023d1c997
superdanvr2013-create <superdanvr2013@gmail.com>
2026-02-04 01:11:12 +0300

    Update chat.lua

--- chat.lua
+++ chat.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 
 local player = Players.LocalPlayer
-local API_URL = "http://asvego.ru/roblox/chat.php"
+local API_URL = "https://asvego.ru/roblox/chat.php"
 local TOKEN = "d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4"
 
 -- ИСПРАВЛЕНИЕ: Используем JobId напрямую как строку. 
