5038e590c730899869a18480f2141e78648cce68
superdanvr2013-create <superdanvr2013@gmail.com>
2026-02-11 12:17:20 +0300

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -3,8 +3,8 @@ local Character = Player.Character or Player.CharacterAdded:Wait()
 local Root = Character:WaitForChild("HumanoidRootPart")
 
 -- === НАСТРОЙКИ ===
-local DIST_UP = 30       -- На сколько студов поднять вверх
-local SPEED_UP = 25       -- Скорость подъема (чем меньше, тем медленнее)
+local DIST_UP = 50       -- На сколько студов поднять вверх
+local SPEED_UP = 35       -- Скорость подъема (чем меньше, тем медленнее)
 -- =================
 
 -- Создаем GUI
